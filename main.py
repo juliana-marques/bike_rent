85bdb39508b5e98e9d46de9eb1586b5ecc9b0449
juliana-marques <99028731+julianamscarneiro@users.noreply.github.com>
2023-10-26 11:51:41 -0300

    f

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import os
 
 app = Flask(__name__)
 
-from .ms_usuario.app_usuario import app_usuario
-app.register_blueprint(app_usuario)
-
-from hello_world import hello_world
-app.register_blueprint(hello_world)
-
+@app.route('/', methods=['GET'])
+def hello_world():
+    try:
+        return "Hello World! :)"
+    except Exception as e:
+        print(f"Error: {str(e)}", file=sys.stderr)
+        return "Internal Server Error", 500
+    
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
-# version 1.0
