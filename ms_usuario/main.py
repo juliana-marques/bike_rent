871a752d023e77382f7350ea184f86bc05576f21
Juliana Marques <115050272+juliana-marques@users.noreply.github.com>
2023-10-26 10:43:34 -0300

    Update main.py

--- ms_usuario/main.py
+++ ms_usuario/main.py
@@ -1,11 +1,11 @@
 from flask import Flask
 import os
 
-app = Flask(__name__)
+app_usuario = Flask(__name__)
 
-@app.route('/ciclista/dados', methods=['GET'])
+@app_usuario.route('/ciclista/dados', methods=['GET'])
 def hello_world():
     return "Ciclista: Fulano\nIdade: Ciclano"
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 5000)),host='0.0.0.0',debug=True)
+    app_usuario.run(port=int(os.environ.get("PORT", 5000)),host='0.0.0.0',debug=True)
