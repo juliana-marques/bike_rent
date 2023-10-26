9d4f6774e7d9c982d68b9cfe51554a551417ea42
Juliana Marques <115050272+juliana-marques@users.noreply.github.com>
2023-10-26 09:01:11 -0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ import sys, os
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
 
-from main import hello_world
+from main import app
 import pytest
 
 @pytest.fixture
