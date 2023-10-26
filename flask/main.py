4a9393b3f20c2c0a3dc2c1fd0271fc7cead3821d
juliana-marques <99028731+julianamscarneiro@users.noreply.github.com>
2023-10-26 07:35:44 -0300

    fix

--- flask/main.py
+++ flask/main.py
@@ -12,4 +12,4 @@ def hello_world():
         return "Internal Server Error", 500
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 5000)),host='0.0.0.0',debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
