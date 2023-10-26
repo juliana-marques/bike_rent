c93aafbde682db529c9de6d713f279a5e2d1aeab
juliana-marques <99028731+julianamscarneiro@users.noreply.github.com>
2023-10-26 11:44:52 -0300

    fix

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ app.register_blueprint(app_usuario)
 from hello_world import hello_world
 app.register_blueprint(hello_world)
 
-
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
-
 # version 1.0
