a9341f7e7ffd6210ff6ea30e56ba829bbc45339f
Juliana Marques <115050272+juliana-marques@users.noreply.github.com>
2023-10-26 07:03:10 -0300

    Update main.py

--- flask/main.py
+++ flask/main.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return "Hello World :)"
+    try:
+        return "Hello World :)"
+    except Exception as e:
+        print(f"Error: {str(e)}", file=sys.stderr)
+        return "Internal Server Error", 500
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
