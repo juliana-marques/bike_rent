e63bc3557fd4f3078c2e86df339bf6a6af9d4ae2
Juliana Marques <115050272+juliana-marques@users.noreply.github.com>
2023-10-25 22:22:20 -0300

    Update main.py

--- flask/main.py
+++ flask/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,4 @@ def hello_world():
     return "Hello World :)"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
