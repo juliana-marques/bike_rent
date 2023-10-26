54d54d3753578aa2de5d8b734596610dfa8947d5
Juliana Marques <115050272+juliana-marques@users.noreply.github.com>
2023-10-26 10:45:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from ms_usuario.main import app_usuario
 import os
 
 app = Flask(__name__)
@@ -10,6 +11,8 @@ def hello_world():
     except Exception as e:
         print(f"Error: {str(e)}", file=sys.stderr)
         return "Internal Server Error", 500
+
+app.register_blueprint(app_usuario, url_prefix='/ms_usuario')
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
