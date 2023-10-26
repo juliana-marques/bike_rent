e38fb8194520aebc3cf6766d42bb34c1b5e2bd39
Juliana Marques <115050272+juliana-marques@users.noreply.github.com>
2023-10-26 10:43:06 -0300

    Update main.py

--- ms_usuario/main.py
+++ ms_usuario/main.py
@@ -3,9 +3,9 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/ciclista/dados', methods=['GET'])
 def hello_world():
-    return "Hello World - Gerenciamento Estação Microsservice:)"
+    return "Ciclista: Fulano\nIdade: Ciclano"
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 4000)),host='0.0.0.0',debug=True)
+    app.run(port=int(os.environ.get("PORT", 5000)),host='0.0.0.0',debug=True)
